68cb83a996cee5d7af7b5d088c254647e37b7907
Jenny Rogers <jrogers1@ufl.edu>
2024-02-04 14:48:21 -0500

    test pass

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
+const {getCityInfo, getJobs}= require('./util.js')
 
+app.use(express.static('public'))
+
+
+app.get('/api/city/:city', async (req, res) => {
+    try {
+        const cityInfo = await getCityInfo(req.params.city)
+        const jobs = await getJobs(req.params.city)
+        if (cityInfo || jobs) {
+            res.status(200).json({ cityInfo, jobs })
+        } else {
+            res.status(404).json({ error: 'API information for a city/job is not found' })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+// INSTRUCTIONS:
+// TODO: import the getCityInfo and getJobs functions from util.js/DONE
 // TODO: Statically serve the public folder
-
 // TODO: declare the GET route /api/city/:city
 // This endpoint should call getCityInfo and getJobs and return
 // the result as JSON.
